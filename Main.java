40f60564670fb1db697da84b7cb316239a3674ea
Mayuri Kapte <74652135+Mayuri9524@users.noreply.github.com>
2024-07-31 19:31:38 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] Arr = {-2,3,4,-1,5,-10,7,2,-5,12};
+        SubArraySum obj = new SubArraySum();
+        System.out.println(obj.subSum(Arr));
+    }
+}
---
+++ SubArraySum.java
@@ -0,0 +1,20 @@
+public class SubArraySum {
+    public int subSum(int[] arr)
+    {
+        int ans=0;
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=arr[i];
+            if(sum>ans)
+            {
+                ans=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return ans;
+    }
+}
